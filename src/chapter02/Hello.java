558ff1b3bd75b4aae4040d56434a2accf1656120
SeongeunJo360 <seongeunj360@gmail.com>
2025-06-24 16:35:45 +0900

    .md file modified commit 8th

---
+++ src/chapter02/Hello.java
@@ -0,0 +1,9 @@
+package chapter02;
+
+public class Hello {
+	//main method
+	public static void main(String[] arge) {
+		System.out.print("Welcome to JAVA!!");
+		System.out.println("환장의 나라 자바 월드에 오신결 환영합니다.");
+	}
+}
