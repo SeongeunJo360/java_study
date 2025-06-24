a02b96349724e45c9c38c5396984d0bdc3c85e66
SeongeunJo360 <seongeunj360@gmail.com>
2025-06-24 13:53:09 +0900

    Initial commit

---
+++ src/chapter02/HelloJava.java
@@ -0,0 +1,18 @@
+package chapter02;
+
+public class HelloJava {
+
+	//메인 메소드 - 실행을 위한 필수 메소드
+	public static void main(String[] args) {
+		System.out.println("Welcome to JavaWorld!!!");
+		
+		/*
+		 * Hello Java를 출력하는 실행문을 주석처리 하고 
+		 * 아래 안녕 자바라고 출력되는 실행문을 추가
+		 */
+		
+		//"안녕 자바"라고 출력되는 실행문
+		System.out.println("안녕 자바");
+	}
+
+}
