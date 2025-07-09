d15b47755bdc414c9ab127df6be4e81b172bc91f
SeongeunJo360 <seongeunj360@gmail.com>
2025-07-09 17:09:21 +0900

    2025-07-09 17:09 JO

--- src/vending_machine_ver1/RestAreaTest.java
+++ src/vending_machine_ver1/RestAreaTest.java
@@ -1,10 +1,9 @@
-package vending_machine_ver;
+package vending_machine_ver1;
 
 public class RestAreaTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new RestArea("평창");
 	}
 
 }
