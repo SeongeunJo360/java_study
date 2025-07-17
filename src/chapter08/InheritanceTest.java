fb4d2f60db53ae4684aac12077cfc139c694cf96
SeongeunJo360 <seongeunj360@gmail.com>
2025-07-17 18:24:23 +0900

    25-07-17 18:24 Jo

--- src/chapter08/InheritanceTest.java
+++ src/chapter08/InheritanceTest.java
@@ -1,27 +1,37 @@
 package chapter08;
 
 public class InheritanceTest {
-	
+
 	public static void main(String[] args) {
-//		Client c1 = new Client();
-//		Person p1 = new Person();
-//		Person p2 = new Person("김유신", "종로구", "010-9876-9876", 12);
-//		Animal dog = new Animal("명수",5,lee);
-//		Animal cat = new Animal("야옹이",6,kim);
-//		
-//		System.out.println("c1.name ==>" +c1.name);
-//		System.out.println("p1.name ==>" +p1.name);
-//		System.out.println("a1.name ==>" +a1.name);
-//		System.out.println("p2.name ==>" +p2.name);
-//		
+//		Client c1 = new Client();  //abstract 클래스이므로 객체 생성 불가!!
+		Person lee = new Person();
+		Person kim = new Person("김유신", "종로구", "010-9876-9876", 12);
+		Animal dog = new Animal("명수", 5, lee);  
+		Animal cat = new Animal("야옹이", 6, kim);  
+		
 //		c1.printInfo();
-//		p1.printInfo();
-//		a1.printInfo();
-//		p2.printInfo();
-//		
-//		dog.sound();
-//		cat.sound();
+		lee.printInfo();
+		kim.printInfo();
+		dog.printInfo();
+		cat.printInfo();
+		
+		lee.register();
+		cat.register();
+		
+		
+		dog.sound();
+		cat.sound();
 
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
