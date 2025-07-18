320e2cecb1f51da95617bd2503c65cb896a18325
SeongeunJo360 <seongeunj360@gmail.com>
2025-07-18 17:12:46 +0900

    25-07-18 17:12 Jo

--- src/com/scoremgm/repository/ScoreRepositoryImpl.java
+++ src/com/scoremgm/repository/ScoreRepositoryImpl.java
@@ -1,12 +1,59 @@
 package com.scoremgm.repository;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
 import com.scoremgm.model.Member;
 
 public class ScoreRepositoryImpl implements ScoreRepository{
 	
 	List<Member> storage = new ArrayList<Member>(); 
+	
+	@Override
+	public void remove(String no) {
+		no = "2025-" + no;
+		Iterator<Member> ie = storage.iterator();
+		while(ie.hasNext()) {
+			Member memeber = ie.next();
+			if(memeber.getNo().equals(no)) {
+				ie.remove();
+				break;
+			}
+		}
+	}
+	
+	
+	@Override
+	public void update(Member member) {
+		int idx = -1;
+		for(int i=0; i<storage.size();i++) {
+			Member m = storage.get(i);
+			if(m.getNo().equals(member.getNo())) {
+				idx = i;
+				break;
+			}
+		}
+		
+		storage.set(idx, member);
+	}
+	
+	
+	@Override
+	public Member find(String no) {
+		no = "2025-" + no;
+		Member member = null;
+		
+		if(no != null) {
+			for(Member m : storage) {
+				if(m.getNo().equals(no)) {
+					member = m;
+				}	
+			}
+		}
+		
+		return member;
+	}
 	
 	@Override
 	public List<Member> findAll() {
